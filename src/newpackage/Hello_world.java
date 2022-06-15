345eac164b2a4cc1976dd62682cf84e6868948a0
adrianolsz <adeczko@gmail.com>
2022-06-15 11:38:11 +0200

    initial commit

---
+++ src/newpackage/Hello_world.java
@@ -0,0 +1,22 @@
+package newpackage;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hello_world {
+
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/");
+        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
+        element.sendKeys("abcd@gmail.com");
+
+        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
+        button.click();
+
+        driver.close();
+    }
+
+
+}
